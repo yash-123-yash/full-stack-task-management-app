94650431fc7feeaeacdd62bd33c3bedda3175b37
yash16160 <yashbhalerao1001@gmail.com>
2025-02-02 18:55:34 +0530

    dskjgfiu

--- be/src/db/index.js
+++ be/src/db/index.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 import { DATABASE_URL } from "../utils/config.js";
 
-const dbString = DATABASE_URL
+const dbString = DATABASE_URL;
+
+const options = {
+    serverApi: {
+      version: '1',
+      strict: true,
+      deprecationErrors: true,
+    }
+  };
 
 export async function connectDB(){
-    await mongoose.connect(dbString)
+    await mongoose.connect(dbString, options)
     return;
 }
